1b3d4e08cfb1bc827cea85405a598009b52cf657
Oliver Brooks <oli@pearlshare.com>
2016-11-11 12:38:47 +0000

    tidy up server

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const fs = require("fs-extra");
 const path = require("path");
 
 const configFilePath = path.resolve(__dirname, ".env");
-if (!fs.existsSync(configFilePath)) {
+if (process.env.NODE_ENV !== "production" && !fs.existsSync(configFilePath)) {
   process.stdout.write("###### Creating .env file for first startup ########\n");
   fs.writeFileSync(configFilePath, fs.readFileSync(path.resolve(__dirname, ".env_bak")));
 }
 
 require("dotenv").config();
 
-require("./lib");
+require("./lib/server");
