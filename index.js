f95ed2e9abe48486935a21c4ccc792b6208552c5
Oliver Brooks <oli@pearlshare.com>
2016-11-11 12:00:32 +0000

    add linting

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const fs = require("fs-extra");
+const path = require("path");
+
+const configFilePath = path.resolve(__dirname, ".env");
+if (!fs.existsSync(configFilePath)) {
+  process.stdout.write("###### Creating .env file for first startup ########\n");
+  fs.writeFileSync(configFilePath, fs.readFileSync(path.resolve(__dirname, ".env_bak")));
+}
+
+require("dotenv").config();
+
 require("./lib");
